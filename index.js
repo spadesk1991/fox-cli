6a34757c13b25d3fbd6e1ce63a869b2c5faf803e
spadesk <1104624312@qq.com>
2018-09-26 00:14:53 +0800

    feat(offe) 增加offe可执行文件启动命令

--- index.js
+++ index.js
@@ -8,50 +8,55 @@ const inquirer = require('inquirer');
 const ora = require('ora');
 const chalk = require('chalk');
 const symbols = require('log-symbols'); //（ 正确  失败 ） 图标
-program.version('1.0.0', '-v, --version').command('init <name>').action((name) => {
-    let flag = fs.existsSync(name);
+program.version('1.0.0', '-v, --version')
+  .command('init <project>').action((project) => {
+    let flag = fs.existsSync(project);
     if (flag) {
-        inquirer.prompt([{
-            name: "flag",
-            message: "project already exists, is it covered?(yes/no ?)"
-        }]).then((answers) => {
-            if (answers.flag === "yes") {
-                flag = false;
-            };
-            if (!flag) { // 项目不存在
-                inquirer.prompt([{
-                    name: 'description',
-                    message: 'project description'
-                }, {
-                    name: 'author',
-                    message: 'author'
-                }]).then((answers) => {
-                    const spinner = ora('loading...');
-                    spinner.start();
-                    download('direct:https://github.com/spadesk1991/fox/archive/master.zip', name, (err) => {
-                        if (err) {
-                            spinner.fail();
-                            console.log(symbols.error, chalk.red(err));
-                        } else {
-                            spinner.succeed();
-                            const fileName = `${name}/package.json`;
-                            const meta = {
-                                name,
-                                description: answers.description,
-                                author: answers.author
-                            }
-                            if (fs.existsSync(fileName)) {
-                                const content = fs.readFileSync(fileName).toString();
-                                const result = handlebars.compile(content)(meta);
-                                fs.writeFileSync(fileName, result);
-                            }
-                            console.log(symbols.success, chalk.green('inited'));
-                        }
-                    })
-                })
-            }
-        });
+      inquirer.prompt([{
+        name: "flag",
+        message: "project already exists, is it covered?(yes/no ?)"
+      }]).then((answers) => {
+        if (answers.flag === "yes") {
+          flag = false;
+        };
+        handle(project);
+      });
+    } else {
+      handle(project);
     }
 
-})
+  })
 program.parse(process.argv);
+
+function handle(project) {
+  inquirer.prompt([{
+    name: 'description',
+    message: 'project description'
+  }, {
+    name: 'author',
+    message: 'author'
+  }]).then((answers) => {
+    const spinner = ora('loading...');
+    spinner.start();
+    download('direct:https://github.com/spadesk1991/fox/archive/master.zip', project, (err) => {
+      if (err) {
+        spinner.fail();
+        console.log(symbols.error, chalk.red(err));
+      } else {
+        spinner.succeed();
+        const fileName = `${project}/package.json`;
+        const meta = {
+          name: project,
+          description: answers.description,
+          author: answers.author
+        };
+        if (fs.existsSync(fileName)) {
+          const content = fs.readFileSync(fileName).toString();
+          const result = handlebars.compile(content)(meta);
+          fs.writeFileSync(fileName, result);
+        }
+        console.log(symbols.success, chalk.green('inited'));
+      }
+    });
+  });
+}
